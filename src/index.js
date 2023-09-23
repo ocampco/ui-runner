108b80192a17dba90e7ec0ef948f34f9b70804e2
ocampco <ocampco@gmail.com>
2023-09-23 21:38:49 +0700

    Allow for routing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
 import './index.css';
-import App from './App';
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
