30246be3dd12096482967e751941ae6ff77d89b9
ocampco <ocampco@gmail.com>
2023-09-24 20:39:28 +0700

    Remove unneccsary comments

--- src/App.js
+++ src/App.js
@@ -1,6 +1,3 @@
-// print current time
-// https://mocki.io/v1/2c2705d8-6fe4-4877-83a0-e6bbc9a395d2
-
 const date = new Date();
 const dateString = date.toLocaleString();
 const App = ({ time = dateString }) => (
