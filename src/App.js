11a08a513925c820dec0bcbbc92ff4688c341760
ocampco <ocampco@gmail.com>
2023-09-24 11:11:45 +0700

    Display time

--- src/App.js
+++ src/App.js
@@ -1,3 +1,13 @@
-const App = () => <h1>Hello World</h1>;
+// print current time
+// https://mocki.io/v1/2c2705d8-6fe4-4877-83a0-e6bbc9a395d2
+
+const date = new Date();
+const dateString = date.toLocaleString();
+const App = ({ time = dateString }) => (
+  <>
+    <h1>Hello World</h1>
+    <div data-testid='time'>{time}</div>
+  </>
+);
 
 export default App;
--- src/__tests__/App.snap.test.js
+++ src/__tests__/App.snap.test.js
@@ -3,7 +3,8 @@ import App from '../App';
 
 describe('App', () => {
   it('should render correctly', () => {
-    const tree = create(<App />).toJSON();
+    const time = '24/09/2023, 11:08:43';
+    const tree = create(<App time={time} />).toJSON();
 
     expect(tree).toMatchSnapshot();
   });
